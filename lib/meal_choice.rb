f7921afd9cd86bd70ffeccf91a46a375bfd4749f
Kelly Small <xcxkelly98@gmail.com>
2019-10-30 21:44:20 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,17 @@
-# Your code here
+puts "{what a nutritious meal!}"
+puts "{A plate of #{protein} with #{veg1} and #{veg2}"
+
+meal_choice(veg1, veg2,)
+protein=meat
+
+veg1= potato
+veg2= carrot
+protein= meat
+
+end
+
+puts: "A plate of meat with potato and carrot"
+end
+
+
+
